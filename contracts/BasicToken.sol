07b6007f925142b04cde31662416cc24b51639a6
Clifton King <cliftonk@gmail.com>
2020-12-15 02:18:54 -0600

    add basic token and test examples

---
+++ contracts/BasicToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Example class - a mock class using delivering from ERC20
+contract BasicToken is ERC20 {
+  constructor(uint256 initialBalance) ERC20("Basic", "BSC") {
+    _mint(msg.sender, initialBalance);
+  }
+}
